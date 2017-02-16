fdf91c11f0a4825603642a3b6638b8d16f94830a
Tristan King <tristan@bakkenbaeck.no>
2017-02-16 16:19:00 +0100

    add uport id creation to faucet

--- app.py
+++ app.py
@@ -1,21 +1,51 @@
 import os
+import binascii
+import logging
 
 from flask import Flask
 from flask import request
 from flask import render_template
 
-from tokenbrowser.utils import validate_address, validate_int_string, validate_decimal_string, parse_int
+from tokenbrowser.utils import validate_address, validate_int_string, validate_hex_string, validate_decimal_string, parse_int
 from tokenbrowser.tx import sign_transaction
 
 from decimal import Decimal
 
+from ethutils import sha3, data_decoder, data_encoder
 from tokenbrowser.ethereum_service_client import EthereumServiceClient
+
+logging.basicConfig()
+log = logging.getLogger("log")
 
 app = Flask(__name__)
 
 ETHEREUM_SERVICE_URL = os.environ['ETHEREUM_SERVICE_URL']
 FAUCET_ADDRESS = os.environ['FAUCET_ADDRESS']
 FAUCET_PRIVATE_KEY = os.environ['FAUCET_PRIVATE_KEY']
+UPORT_ID_FACTORY_ADDRESS = os.environ['UPORT_ID_FACTORY_ADDRESS']
+
+def process_tx_args():
+    tx_args = {}
+    if 'startgas' in request.form:
+        startgas = request.form['startgas'].strip()
+        if startgas:
+            startgas = parse_int(startgas)
+            if startgas:
+                tx_args['gas'] = startgas
+            else:
+                raise Exception("Invalid value for startgas")
+    if 'gasprice' in request.form:
+        gasprice = request.form['gasprice'].strip()
+        if gasprice:
+            if validate_decimal_string(gasprice):
+                gasprice = None
+            else:
+                gasprice = parse_int(gasprice)
+            if gasprice:
+                tx_args['gas_price'] = gasprice
+            else:
+                raise Exception("Invalid value for gasprice")
+    return tx_args
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
@@ -26,37 +56,68 @@ def main():
     args = {'faucet_address': FAUCET_ADDRESS, 'available_ethereum': Decimal(unconfirmed) / 10 ** 18}
 
     if request.method == 'POST':
-        print(request.form['address'], request.form['value'])
-        if not all(x in request.form for x in ['address', 'value']):
-            args['error'] = "Missing required arguments"
-        elif not validate_address(request.form['address'].strip()):
-            args['error'] = "Invalid Address"
-        if not (validate_int_string(request.form['value'].strip()) or validate_decimal_string(request.form['value'].strip())):
-            args['error'] = "Invalid Eth amount"
-        else:
-            address = request.form['address'].strip()
-            value = request.form['value'].strip()
-            if validate_int_string(value):
-                value = parse_int(value)
+        print(request.form)
+        if 'uport' in request.form:
+            if not all(x in request.form for x in ['address']):
+                args['error'] = "Missing required arguments"
             else:
-                value = Decimal(value)
 
-            wei = int(value * 10 ** 18)
+                try:
+                    tx_args = process_tx_args()
+                    func = sha3('CreateProxyWithControllerAndRecovery(address,address[],uint256,uint256)')[:4]
+                    device_addr = data_decoder(request.form['address'].strip()).rjust(32, b'\x00')
+                    offset = b'\x80'.rjust(32, b'\x00')  # offset of address[] data
+                    longtimeval = b'\x3c'.rjust(32, b'\x00')
+                    shorttimeval = b''.rjust(32, b'\x00')
+                    delegate_data = b''.rjust(32, b'\x00')
+                    data = func + device_addr + offset + longtimeval + shorttimeval + delegate_data
+                    tx_args['data'] = data_encoder(data)
 
-            try:
-                tx = client.generate_tx_skel(FAUCET_ADDRESS, address, wei)
-                tx_hash = client.send_tx(sign_transaction(tx, FAUCET_PRIVATE_KEY))
-                args['tx_hash'] = tx_hash
-                args['success'] = True
+                    tx = client.generate_tx_skel(FAUCET_ADDRESS, UPORT_ID_FACTORY_ADDRESS, 0, **tx_args)
+                    tx_hash = client.send_tx(sign_transaction(tx, FAUCET_PRIVATE_KEY))
+                    args['tx_hash'] = tx_hash
+                    args['success'] = True
+                    args['uport'] = True
 
-            except Exception as e:
-                args['error'] = str(e)
+                except binascii.Error:
+                    args['error'] = "Invalid address"
+                except Exception as e:
+                    log.exception("Unexpected exception")
+                    args['error'] = str(e)
+        else:
+            if not all(x in request.form for x in ['address', 'value']):
+                args['error'] = "Missing required arguments"
+            elif not validate_address(request.form['address'].strip()):
+                args['error'] = "Invalid Address"
+            if not (validate_int_string(request.form['value'].strip()) or validate_decimal_string(request.form['value'].strip())):
+                args['error'] = "Invalid Eth amount"
+            else:
+                address = request.form['address'].strip()
+                value = request.form['value'].strip()
+                if validate_int_string(value):
+                    value = parse_int(value)
+                    wei = int(value * 10 ** 18)
+                elif validate_hex_string(value):
+                    wei = parse_int(value)
+                else:
+                    value = Decimal(value)
+                    wei = int(value * 10 ** 18)
 
-        args.update({k: request.form[k] for k in request.form.keys()})
+                try:
+                    tx_args = process_tx_args()
+                    tx = client.generate_tx_skel(FAUCET_ADDRESS, address, wei, **tx_args)
+                    tx_hash = client.send_tx(sign_transaction(tx, FAUCET_PRIVATE_KEY))
+                    args['tx_hash'] = tx_hash
+                    args['success'] = True
+
+                except Exception as e:
+                    args['error'] = str(e)
+
+            args.update({k: request.form[k] for k in request.form.keys()})
 
     elif request.method == 'GET':
 
-        args['address'] = request.args.get('address')
+        args['address'] = request.args.get('address', '')
         value = request.args.get('wei')
         if value:
             value = parse_int(value)
